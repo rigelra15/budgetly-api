4bdff9319c06c9df87468c2a487b9f33542fbf0b
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2025-01-01 15:17:05 +0700

    fix: remove databaseURL from server.js

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const admin = require('firebase-admin')
 const bodyParser = require('body-parser')
 const path = require('path')
 
+var serviceAccount = require('./budgetly-app-dcdg-firebase-adminsdk-lsmep-b84b393414.json')
+
 // Inisialisasi Firebase
 admin.initializeApp({
-	credential: admin.credential.cert(
-		require('./budgetly-app-dcdg-firebase-adminsdk-lsmep-b84b393414.json')
-	),
-	databaseURL: 'https://budgetly-app-dcdg.firebaseio.com',
+	credential: admin.credential.cert(serviceAccount),
 })
 
 // Import modul
@@ -32,6 +31,16 @@ app.use('/api/transactions', transactionRoutes)
 app.use('/api/budgets', budgetRoutes)
 app.use('/api/savings', savingsRoutes)
 app.use('/api/ai', aiRoutes)
+
+app.get('/debug', async (req, res) => {
+	try {
+		const users = await admin.auth().listUsers(10) // Ambil 10 pengguna pertama
+		res.json({ message: 'Firebase SDK terhubung!', users })
+	} catch (error) {
+		console.error('Error Firebase SDK:', error)
+		res.status(500).json({ error: 'Firebase SDK tidak terhubung.' })
+	}
+})
 
 // Jalankan server
 app.listen(PORT, () => {
